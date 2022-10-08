6200ee3de51fa1351f99514d80ab4d0664d1fceb
pickeloe5 <aboutthepickle@gmail.com>
2022-10-08 06:01:50 -0400

    Nuked all my hard work.

--- public/js/fuffle/Util.js
+++
@@ -1,7 +0,0 @@
-export const cloneTemplate = template =>
-  [...template.content.cloneNode(true).childNodes]
-
-export const appendTemplate = (parent, template) => {
-  for (const child of cloneTemplate(template))
-    parent.appendChild(child)
-}
