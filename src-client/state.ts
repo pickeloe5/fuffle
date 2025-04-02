989f4d80324e0028d52dbc904d369df7507ffaa9
Nic Mattiacci <aboutthepickle@gmail.com>
2025-04-02 10:19:34 -0400

    Added case for simple object state.

--- src-client/state.ts
+++ src-client/state.ts
@@ -15,6 +15,88 @@ export class StateWrapper<T> {
     }
 }
 
+export class ObjectStateWrapper<T extends object> {
+    state: T
+    listeners: ObjectStateListener<T>[]
+    constructor(state: T) {
+        this.state = state
+    }
+    read(callback: (state: T) => void) {
+        const listener = new ObjectStateListener(this, callback)
+        this.listeners.push(listener)
+        listener.fire()
+    }
+    write(state: T) {
+        const keys: ObjectStateKey[] = []
+        for (const key in this.state)
+            if (state[key] !== this.state[key])
+                keys.push(key)
+        for (const key in state)
+            if (key !in this.state)
+                keys.push(key)
+        this.state = state
+        for (const listener of this.listeners)
+            if (listener.check(keys))
+                listener.fire()
+    }
+}
+
+export class ObjectStateListener<T extends object> {
+    #stateWrapper: ObjectStateWrapper<T>
+    #callback: (state: T) => void
+    #dependencies: ObjectStateKey[] = []
+    constructor(
+        stateWrapper: ObjectStateWrapper<T>,
+        callback: (state: T) => void
+    ) {
+        this.#stateWrapper = stateWrapper
+        this.#callback = callback
+    }
+    fire() {
+        const observer = new ObjectStateObserver<T>(this.#stateWrapper.state)
+        this.#callback(observer.proxy)
+        observer.stop()
+        this.#dependencies = observer.getDependencies()
+    }
+    check(keys: ObjectStateKey[]) {
+        return !keys.some(key => this.#dependencies.includes(key))
+    }
+}
+
+type ObjectStatePath = ObjectStateKey[]
+type ObjectStateKey = string | number
+
+class ObjectStateObserver<T extends object> {
+    proxy: T
+    stop: () => void
+    #gets: ObjectStateKey[] = []
+    constructor(state: T) {
+        const {proxy, revoke} = this.#makeProxy(state)
+        this.proxy = proxy
+        this.stop = revoke
+    }
+    getDependencies() {
+        return [...this.#gets]
+    }
+    #onGet(key: ObjectStateKey) {
+        if (!this.#gets.includes(key))
+            this.#gets.push(key)
+    }
+    #makeProxy(state: T) {
+        return Proxy.revocable(state, {
+            get: (target, key, receiver) => {
+                if (typeof key === 'string')
+                    this.#onGet(key)
+                return Reflect.get(target, key, receiver)
+            }
+        })
+    }
+}
+
 export default function state<T>(value: T): StateWrapper<T> {
     return new StateWrapper(value)
+}
+
+export function objectState<T extends object>(value: T): ObjectStateWrapper<T> {
+    return new ObjectStateWrapper(value)
 }
--- test-client/index.js
+++ test-client/index.js
@@ -7,4 +7,10 @@ addEventListener('load', () => {
     stateWrapper.read(state => {node.textContent = state})
     document.body.appendChild(node)
     setTimeout(() => {stateWrapper.write('bye!')}, 1000)
+
+    const objectStateWrapper = Fuffle.state({a: 'a1', b: 'b1'})
+    const node2 = document.createTextNode('')
+    objectStateWrapper.read(state => {node2.textContent = state.a})
+    document.body.appendChild(node2)
+    setTimeout(() => {objectStateWrapper.write({a: 'a2', b: 'b1'})}, 1000)
 })
